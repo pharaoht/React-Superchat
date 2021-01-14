073f151c4df481d4b4bad44673c759a46b417593
pharaoh <pharaohmanson@gmail.com>
2021-01-13 20:29:23 -0500

    Added Google auth and firebase connection to the project

--- superchat/src/App.js
+++ superchat/src/App.js
@@ -7,21 +7,52 @@ import { useAuthState } from 'react-firebase-hooks';
 import { useCollectionData } from 'react-firebase-hooks/firestore';
 
 firebase.initializeApp({
-
+  apiKey: "AIzaSyDr7pbZUS9bBy7bJxviYjUQzzC-Ta36QLc",
+  authDomain: "super-chat-e2378.firebaseapp.com",
+  projectId: "super-chat-e2378",
+  storageBucket: "super-chat-e2378.appspot.com",
+  messagingSenderId: "949858675008",
+  appId: "1:949858675008:web:c98738b231cd49e7bf72a5"
 })
 
 const auth = firebase.auth();
 const firestore = firebase.firestore();
+const [user] = useAuthState(auth);
 
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header >
 
       </header>
+      <section>
+        {user ? <ChatRoom/> : <SignIn/>}
+      </section>
     </div>
   );
+}
+
+function SignIn(){
+  const signInWithGoogle = () =>{
+    const provider = new firebase.auth.GoogleAuthProvider();
+    auth.signInWithPopup(provider);
+  }
+  return(
+    <button onClick={signInWithGoogle}>Sign in with Google</button>
+  )
+}
+
+function SignOut() {
+  return auth.currentUser && (
+    <button onClick={() => auth.signOut()}Sign Out></button>
+  )
+}
+
+function ChatRoom(){
+  const messagesRef = firestore.collection('messages');
+  const query = messages.orderBy('createdAt').limit(25);
+
 }
 
 export default App;
